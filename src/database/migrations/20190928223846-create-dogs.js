d7c39112455c8fb95ac333064d4b10daf57c4458
Daniel Dutra <danieldhsd@gmail.com>
2019-09-30 09:36:51 -0300

    Crianda tabela do banco de dados

--- src/database/migrations/20190928223846-create-dogs.js
+++ src/database/migrations/20190928223846-create-dogs.js
@@ -2,16 +2,143 @@
 
 module.exports = {
   up: (queryInterface, DataTypes) => {
-    return queryInterface.createTable();
+    return queryInterface.createTable('Animal', {
+      num_controle: {
+        allowNull: false,
+        type: DataTypes.BIGINT(10),
+        primaryKey: true,
+        autoIncrement: false,
+        unique: true,
+      },
+      horario: {
+        allowNull: false,
+        type: DataTypes.DATE_TIME,
+      },
+      nome_proprietario: {
+        allowNull: true,
+        type: DataTypes.STRING,
+      },
+      rua: {
+        allowNull: true,
+        type: DataTypes.STRING,
+      },
+      numero: {
+        allowNull: true,
+        type: DataTypes.INTEGER,
+      },
+      bairro: {
+        allowNull: true,
+        type: DataTypes.ENUM,
+        values: [
+          'Açude',
+          'Amaral',
+          'Aparecida',
+          'Bela Vista',
+          'Boa Vista',
+          'Bom Jardim das Pedras',
+          'Cachoeirinha',
+          'Cacimba',
+          'Centro',
+          'Corrego da Prata',
+          'Fátima',
+          'Glória',
+          'Graminha',
+          'Japão Grande',
+          'Jardim América',
+          'Lava-Pés',
+          'Lourdes',
+          'Padre José Erlei',
+          'Parque Industrial',
+          'Santo Antônio',
+        ],
+      },
+      telefone: {
+        allowNull: true,
+        type: DataTypes.STRING(12),
+      },
+      nome_animal: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      genero: {
+        allowNull: false,
+        type: DataTypes.ENUM,
+        values: ['Fêmea', 'Macho'],
+      },
+      idade: {
+        allowNull: true,
+        type: DataTypes.INT,
+      },
+      raca: {
+        allowNull: true,
+        type: DataTypes.STRING,
+      },
+      cor: {
+        allowNull: true,
+        type: DataTypes.STRING,
+      },
+      pelagem: {
+        allowNull: true,
+        type: DataTypes.STRING,
+      },
+      porte: {
+        allowNull: false,
+        type: DataTypes.ENUM,
+        values: ['Pequeno', 'Medio', 'Grande'],
+      },
+      observacoes: {
+        allowNull: true,
+        type: DataTypes.STRING(300),
+      },
+      teste_rapido: {
+        allowNull: false,
+        type: DataTypes.ENUM,
+        values: ['Positivo', 'Negativo', 'Indeterminado'],
+      },
+
+      // Somente se o teste rápido for positivo
+      data_coleta_sangue: {
+        allowNull: true,
+        type: DataTypes.DATE_TIME,
+      },
+      data_cadastro_gal: {
+        allowNull: true,
+        type: DataTypes.DATE_TIME,
+      },
+      numero_cadastro_gal: {
+        allowNull: true,
+        type: DataTypes.BIGINT(10),
+      },
+      resultado_teste_gal: {
+        allowNull: true,
+        type: DataTypes.ENUM,
+        values: ['Positivo', 'Negativo', 'Indeterminado', 'Recusa'],
+      },
+
+      // Somente se o resultado do teste do Gal for positivo
+      decisao: {
+        allowNull: true,
+        type: DataTypes.ENUM,
+        values: ['Eutanasia', 'Tratamento', 'Recusa', 'Morreu', 'Fugiu'],
+      },
+
+      // Somente se a decisao for pela eutanasia
+      data_eutanasia: {
+        allowNull: true,
+        type: DataTypes.DATE_TIME,
+      },
+      veterinario_resp_eutanasia: {
+        allowNull: true,
+        type: DataTypes.STRING,
+      },
+      observacoes_eutanasia: {
+        allowNull: true,
+        type: DataTypes.STRING(300),
+      },
+    });
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return queryInterface.dropTable('dogs');
   },
 };
