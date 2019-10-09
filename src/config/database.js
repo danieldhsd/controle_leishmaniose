81ba68ee2fbe57d3ac81e17bff40ff04df054d5d
Daniel Dutra <danieldhsd@gmail.com>
2019-10-09 15:43:11 -0300

    Bug fix

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,9 @@
-require('dotenv/config');
-
 module.exports = {
   dialect: 'postgres',
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: 'ec2-54-204-37-92.compute-1.amazonaws.com',
+  username: 'kawjutftxehlra',
+  password: '80fb8acb573c7d7ef91112ca3cb86865e1df92d1bb157f691d6d0f9d983dde38',
+  database: 'd68vdmf94l3sfp',
   operatorAliases: false,
   define: {
     timestamps: true,
